716427e11365c715ae95213becad34c9f2c6a717
gitSanjay94 <sanjay.das@sleepcountry.ca>
2025-06-08 23:00:21 +0530

    8 june

---
+++ tests/Module05/05_PageObjectModelTest.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test'
+import { HomePage } from '../../src/pages/HomePage'
+import { ResultPage } from '../../src/pages/ResultPage';
+import { PlaylistPage } from '../../src/pages/PlaylistPage';
+
+test('Page Object Model Test', async ({ page }) => {
+test.setTimeout(120000);
+    // Create object of homepage
+    const homepage = new HomePage(page);
+    await homepage.goToURL();
+    await homepage.searchWithKeywords(`${process.env.SEARCH_KEYWORDS}`);
+
+    // Create object of resultpage
+    const resultpage = new ResultPage(page);
+    await page.waitForTimeout(8000);
+    await resultpage.clickOnPlaylist(`${process.env.SEARCH_KEYWORDS}`);
+
+    // Create object of playlistpage
+    const playlistpage = new PlaylistPage(page);
+     await page.waitForTimeout(8000);
+    await playlistpage.validatePageTitle(`${process.env.SEARCH_KEYWORDS}` + '☑️ - YouTube');
+})
