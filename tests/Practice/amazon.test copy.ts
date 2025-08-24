f505e365c5dfd8a976c149b2150e1a2318e053dd
gitSanjay94 <sanjay.das@sleepcountry.ca>
2025-08-24 22:14:54 +0530

    API

--- tests/Practice/amazon.test copy.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("amazon", async ({ page }) => {
-    test.setTimeout(120000);
-    await page.goto("playwrightTypeScript/tests/Practice/amazon.test.ts")
-   
-})
